0b9d0ca15601f2c65604650e3b21ef33cee1de74
MrHeaven1y <dibyendumukherjee916@gmail.com>
2026-06-14 22:16:11 +0530

    Auto update 2026-06-14 22:16:11

--- module-A/app/schemas.py
+++ module-A/app/schemas.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field 
 from typing import Dict, Any, Optional
 
 
-class SimulationConfig(BaseModel):
-        name: str = Field(..., description="Human-friendly name")
-        steps: int = Field(100, ge=1, le=10000)
-        complexity: int = Field(3, ge=1, le=10)
-        params: Dict[str, Any] = {}
+class SimulationConfig(BaseModel): # startup simulation config class
+        name: str = Field(..., description="Human-friendly name") # simulation name
+        steps: int = Field(100, ge=1, le=10000) # number steps per unit time, basically how many divison of total fake time 
+        complexity: int = Field(3, ge=1, le=10) # resolution control
+        params: Dict[str, Any] = {} # addition parameters
 
 class StartResponse(BaseModel):
     run_id: str
