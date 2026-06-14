a6a4fc8124b6ebe9144d3dea36080c7934bafced
MrHeaven1y <dibyendumukherjee916@gmail.com>
2026-06-14 22:25:57 +0530

    Auto update 2026-06-14 22:25:57

--- module-A/app/schemas.py
+++ module-A/app/schemas.py
@@ -4,12 +4,12 @@ from typing import Dict, Any, Optional
 
 class SimulationConfig(BaseModel): # startup simulation config class
         name: str = Field(..., description="Human-friendly name") # simulation name
-        steps: int = Field(100, ge=1, le=10000) # number steps per unit time, basically how many divison of total fake time 
-        complexity: int = Field(3, ge=1, le=10) # resolution control
-        params: Dict[str, Any] = {} # addition parameters
+        steps: int = Field(100, ge=1, le=10000)     # Total number of steps (how many divisions of total fake time)
+        complexity: int = Field(3, ge=1, le=10)     # Level of detail / computational intensity per step
+        params: Dict[str, Any] = {}                 # additional parameters
 
 class StartResponse(BaseModel):
-    run_id: str
+    run_id: str # 
 
 
 class StatusResponse(BaseModel):
