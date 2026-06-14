a92e3e604b77e35f2fc04c8906198eaac9278e45
MrHeaven1y <dibyendumukherjee916@gmail.com>
2026-06-14 14:34:58 +0530

    Auto update 2026-06-14 14:34:58

--- module-A/app/schemas.py
+++ module-A/app/schemas.py
@@ -1,12 +1,28 @@
 from pydantic import BaseModel, Field, conint
 from typing import Dict, Any, Optional
 
+StepsType = conint(ge=1, le=10000)
+ComplexityType = conint(ge=1, le=10)
+
+
 class SimulationConfig(BaseModel):
-    
-    name: str = Field(..., description="Human-friendly name")
-    steps: conint(ge=1, le=10000) = 100
-    complexity: conint(ge=1, le=10) = 3
-    params: Dict[str, Any] = {}
+        name: str = Field(..., description="Human-friendly name")
+        steps: StepsType = 100
+        complexity: ComplexityType = 3
+        params: Dict[str, Any] = {}
 
 class StartResponse(BaseModel):
     run_id: str
+
+
+class StatusResponse(BaseModel):
+    run_id: str
+    status: str
+    progress: float
+    started_at: Optional[str]
+    finished_at: Optional[str]
+    error: Optional[str]
+
+class ResultResponse(BaseModel):
+    run_id: str
+    resutl: Dict[str, Any]
